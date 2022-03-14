6e867ab9b0cd0bcd0032bb9b5dcd146e213e83d2
leomonay <leonardomonay@gmail.com>
2022-03-14 14:54:01 -0300

    link to profile

--- src/App.svelte
+++ src/App.svelte
@@ -18,15 +18,20 @@
 </style>
 
 <div class='hero is-fullheight'>
-	<section class="hero is-primary is-small">
-		<div class="hero hero-body">
-				<p class="title">
+	<section class="hero is-primary is-small pb-0">
+		<div class="hero hero-header has-text-centered">
+				<p class="title m-0">
 					Compu-Global-Hyper-Mega-Net
 				</p>
-		<div class="hero hero-footer">			
-				<p class="subtitle">
-					Welcome, {name.toUpperCase()}!
+		<div class="hero hero-body p-0">			
+				<p class="is-size-7	">
+					<a href='https://leomonay.github.io/' target='_blank' rel="noopener noreferrer">Leo Monay's</a> Svelte, Bulma.io & TypeScript first project
 				</p>
+		</div>
+		<div class="hero hero-body p-3">			
+			<p class="subtitle p-0">
+				Welcome, {name.toUpperCase()}!
+			</p>
 		</div>
 	</section>
 	<div class="column bg-img m-0 is-flex is-justify-content-right">
